e8c414ad4578c724ce73a12fa63fcf95bb9e71c8
Martim Tiburcio <tiburciomartim@gmail.com>
2024-09-06 07:57:38 -0300

    Finalizando modulo

--- assets/scripts/cart.js
+++ assets/scripts/cart.js
@@ -1,37 +1,120 @@
-let valorTotal = [0, 0];
-let valorProduto = [50.00, 30.00];
-let qtd = [0, 0];
+let carrinho = [];
+
+function start() {
+    fetch('https://fakestoreapi.com/products?limit=4')
+        .then(response => response.json())
+        .then(data => {
+            preencherCarrinho(data)
+        })
+        .catch(error => {
+            console.log("Ocorreu um erro", error);
+
+        })
+}
+
+function preencherCarrinho(produtos) {
+    produtos.forEach(data => {
+        adicionarAoCarrinho(
+            data.title,
+            data.image,
+            data.description,
+            data.price
+        )
+    });
+
+    atualizarCarrinho();
+}
+
+function adicionarAoCarrinho(nome, imgLink, descricao, preco) {
+    carrinho.push({
+        "nome": nome,
+        "imgLink": imgLink,
+        "descricao": descricao,
+        "preco": Number.parseFloat(preco),
+        "quantidade": 0,
+        "total": 0.0
+    })
+}
+
+function atualizarCarrinho(params) {
+    let carrinhoHTML = document.getElementById("carrinho");
+
+    carrinho.forEach((produto, index) => {
+        `
+            <div id="produto${index}"
+                    class="d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile">
+                </div>
+                <div class="d-flex flex-row align-items-center">
+                    <div>
+                        <img src="${produto.imgLink}" width="150" height="150" alt="" id="image">
+                    </div>
+                    <div class="d-flex flex-column pl-md-3 pl-1">
+                        <div>
+                            <h6>${produto.nome}</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="pl-md-0 pl-1"><b>${produto.preco}</b></div>
+                <div class="pl-md-0 pl-2">
+                    <span class="fa fa-minus-square text-secondary" onclick="removerItem(${index})"></span>
+                    <span class="px-md-3 px-1" id="quantidade${index}">${produto.quantidade}</span>
+                    <span class="fa fa-plus-square text-secondary" onclick="adicionarItem(${index})"></span>
+                </div>
+                <div class="pl-md-0 pl-1"><b>R$</b><span id="total${index}"></span></div>
+                <div class="close" onclick="removeProduto(${index})"></div>
+            </div>
+        `
+    })
+}
 
 function adicionarItem(item) {
-    let quantidade = document.getElementById("quantidade" + item);
-    let total = document.getElementById("total" + item);
-    qtd[item] += 1;
-    valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
-    quantidade.innerHTML = qtd[item];
-    total.innerHTML = valorTotal[item].toFixed(2);
-    valorCompra()
+    let qtd = document.getElementById("quantidade" + item)
+    let produto = carrinho[item];
+    produto.quantidade += 1;
+    qtd.innerHTML = produto.quantidade
 }
 
 function removerItem(item) {
-    if (qtd[item] > 0) {
-        let quantidade = document.getElementById("quantidade" + item);
-        let total = document.getElementById("total" + item);
-        qtd[item] -= 1;
-        valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
-        quantidade.innerHTML = qtd[item];
-        total.innerHTML = valorTotal[item].toFixed(2);
-        valorCompra()
+    let qtd = document.getElementById("quantidade" + item)
+    let produto = carrinho[item];
+    if (produto.quantidade > 0) {
+        produto.quantidade -= 1;
+        qtd.innerHTML = produto.quantidade
+        atualizaNumerosTela(qtd, item)
     }
 }
 
-function valorCompra() {
-    let valorTotalCompra = document.getElementById("valorTotalCompra");
-    let valor = 0;
-
-    for (let i = 0; i < valorTotal.length; i++) {
-        valor += valorTotal[i];
-    }
-
-    valorTotalCompra.innerHTML = valor.toFixed(2);
+function atualizaNumerosTela(qtd, item) {
+    qtd.innerHTML = carrinho[item].quantidade;
+    atualizaTotalProduto(item);
+    atualizaSubtotal();
 }
 
+function atualizaTotalProduto(item) {
+    let total = document.getElementById("total" + item);
+    let produto = carrinho[item];
+    produto.total = Number.parseFloat(
+        produto.quantidade * produto.preco
+    )
+    total.innerHTML = produto.total.toFixed(2)
+}
+
+function atualizaSubtotal() {
+    let totalCompra = document.getElementById("valorTotalCompra");
+    let subtotal = 0;
+    carrinho.forEach(produto => {
+        subtotal += produto.quantidade * produto.preco
+    })
+    totalCompra.innerHTML = subtotal.toFixed(2);
+}
+
+function removeProduto(item) {
+    let carrinhoHTML = document.getElementById("carrinho");
+    let produto = document.getElementById("produto" + item)
+
+    if (confirm("Você tem certeza que deseja excluir o produto?")) {
+        carrinho[item].quantidade = 0;
+        atualizaSubtotal();
+        carrinho.removeChild(produto);
+    }
+}
